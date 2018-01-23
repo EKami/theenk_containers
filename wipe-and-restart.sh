f7fc16f35549ce22b3021f63b1d05edebca7447d
Tuatini <tuatinigodard@gmail.com>
2018-01-23 12:57:02 +0100

    Remove exposed ports

--- wipe-and-restart.sh
+++ wipe-and-restart.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-dir=$(pwd)
-if [[ $UID != 0 ]]; then
-    echo "Please run this script with sudo:"
-    echo "sudo $0 $*"
-    exit 1
-fi
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 echo -e "${BLUE}Killing old instances...${NC}"
-sudo docker rm -f $(sudo docker ps -a -q)
+docker rm -f $(docker ps -a -q)
 echo -e "${BLUE}Wiping folders contents...${NC}"
 rm -rf ./db-data/* 
 rm -rf ./wordpress/*
 echo -e "${BLUE}Running compose...${NC}"
-sudo ./docker-compose up -d
+./docker-compose up -d
