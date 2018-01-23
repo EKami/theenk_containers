f055eb90774a3c2ff221df87e05c62839d266dc7
Tuatini <tuatinigodard@gmail.com>
2018-01-23 13:54:11 +0100

    Update

--- wipe-and-restart.sh
+++ wipe-and-restart.sh
@@ -6,7 +6,7 @@ NC='\033[0m' # No Color
 echo -e "${BLUE}Killing old instances...${NC}"
 docker rm -f $(docker ps -a -q)
 echo -e "${BLUE}Wiping folders contents...${NC}"
-rm -rf ./db-data/* 
-rm -rf ./wordpress/*
+sudo rm -vfR ./db-data/* 
+sudo rm -vfR ./wordpress/*
 echo -e "${BLUE}Running compose...${NC}"
 ./docker-compose up -d
