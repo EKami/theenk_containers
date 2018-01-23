93232c1f8954b39d969f3ef7cee743945b409a0d
Tuatini <tuatinigodard@gmail.com>
2018-01-23 12:05:08 +0100

    Update wipe_and_restart

--- wipe-and-restart.sh
+++ wipe-and-restart.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+dir=$(pwd)
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 echo -e "${BLUE}Killing old instances...${NC}"
 sudo docker rm -f $(sudo docker ps -a -q)
 echo -e "${BLUE}Wiping folders contents...${NC}"
