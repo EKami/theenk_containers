44d0e4c24e963242f3af687fd3116a5dd65f6a6b
Ubuntu <ubuntu@ip-172-31-40-188.eu-west-1.compute.internal>
2018-01-25 14:03:05 +0000

    Remove certbot-nginx

--- runThisOnHost.sh
+++ runThisOnHost.sh
@@ -19,7 +19,7 @@ sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
 sudo apt-get install -y docker-ce
-sudo apt-get install python-certbot-nginx 
+sudo apt-get install certbot
 
 echo -e "${BLUE}Adding certbot renew to crontab...${NC}"
 rm -rf ./letsencrypt/launch_renewal.sh
