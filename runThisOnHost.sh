857477e78b97d6223596ddfa515e418480f8dadc
Ubuntu <ubuntu@ip-172-31-40-188.eu-west-1.compute.internal>
2018-01-25 13:35:09 +0000

    Backing-up entire letsencrypt folder

--- runThisOnHost.sh
+++ runThisOnHost.sh
@@ -14,8 +14,15 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
 sudo apt-get install -y docker-ce
+sudo apt-get install python-certbot-nginx 
+
+echo -e "${BLUE}Restoring certbot config...${NC}"
+sudo rm -rf /etc/letsencrypt
+sudo cp -fR ./letsencrypt /etc/
 
 echo -e "${BLUE}Running compose...${NC}"
 sudo ./docker-compose up -d
